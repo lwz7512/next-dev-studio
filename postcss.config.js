97b4719cd62b9319cbdbd56b8ec579cbf08f7956
wenzhi.li <wenzhi.li@verbinteractive.com>
2021-06-03 08:51:10 -0300

    purge css

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,26 @@
 module.exports = {
   plugins: {
+    autoprefixer: {},
+    "postcss-flexbugs-fixes": {},
     "postcss-import": {},
     "postcss-preset-env": {
-      browsers: "last 2 versions"
+      "browsers": "last 2 versions",
+      "autoprefixer": {
+        "flexbox": "no-2009"
+      },
+      "stage": 3,
+      "features": {
+        "custom-properties": false
+      }
     },
     "postcss-custom-media": {},
-    autoprefixer: {},
+    "@fullhuman/postcss-purgecss": {
+      content: [
+          './pages/**/*.{js,jsx,ts,tsx}',
+          './components/**/*.{js,jsx,ts,tsx}'
+      ],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+      safelist: ["html", "body"]
+    },
   },
 }
